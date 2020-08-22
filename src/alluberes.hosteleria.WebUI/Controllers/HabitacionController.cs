840f761b38973a4c6f552fe2b5f9192e74d08373
Abnel Lluberes Mustafa <alluberes@gmail.com>
2020-08-22 18:07:08 -0400

    source code

---
+++ src/alluberes.hosteleria.WebUI/Controllers/HabitacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace alluberes.hosteleria.WebUI.Controllers
+{
+    public class HabitacionController : Controller
+    {
+        // GET: Habitacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/alluberes.hosteleria.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="alluberes.hosteleria.WebUI.MvcApplication" Language="C#" %>
